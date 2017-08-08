ced4b0970a4ab08a438728389f905095b7a5ed50
mattyice89 <medwards1989@gmail.com>
2017-08-07 22:33:44 -0700

    latest files
    lalalala

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# Get the animal at 1
+print("1. The animal at 1 is a", animals[1])
+print("2. The third (3rd) animal is at 2 and is a", animals[2])
+print("3. The first (1st) animal is at 0 and is a", animals[0])
+print("4. The animal at 3 is a", animals[3])
+print("5. The Fifth (5th) animal is at 4 and is a", animals[4])
+print("6. The animal at 2 is a", animals[2])
+print("7. The sixth (6th) animal is a", animals[5])
+print("8. The animal at 4 is a", animals[4])
